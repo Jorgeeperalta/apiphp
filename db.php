818f5ec51e52f0484409215be03675e0f1a474f4
Jorgeeperalta <edudela36@gmail.com>
2024-03-10 17:57:57 -0300

    domingo 10

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 
 
-//$dbConn = mysqli_connect('localhost','root','root','roytuts') or die('MySQL connect failed. ' . mysqli_connect_error());
-$dbConn = mysqli_connect('MYSQL5032.site4now.net','a47d48_lavalle','afm123**','db_a47d48_lavalle') or die('MySQL connect failed. ' . mysqli_connect_error());
+$dbConn = mysqli_connect('localhost','root','root','m_s_password') or die('MySQL connect failed. ' . mysqli_connect_error());
+//$dbConn = mysqli_connect('MYSQL5032.site4now.net','a47d48_lavalle','afm123**','db_a47d48_lavalle') or die('MySQL connect failed. ' . mysqli_connect_error());
 function dbQuery($sql) {
 	global $dbConn;
 	$result = mysqli_query($dbConn, $sql) or die(mysqli_error($dbConn));
